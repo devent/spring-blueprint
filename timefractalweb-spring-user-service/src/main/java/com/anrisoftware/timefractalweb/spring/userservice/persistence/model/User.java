ad33b1335f6e588dc37b792e4743c404887de2d0
Erwin Müller <erwin.mueller@deventm.org>
2018-11-19 13:13:42 +0100

    Create spring-user-service module.

---
+++ timefractalweb-spring-user-service/src/main/java/com/anrisoftware/timefractalweb/spring/userservice/persistence/model/User.java
@@ -0,0 +1,129 @@
+package com.anrisoftware.timefractalweb.spring.userservice.persistence.model;
+
+/*-
+ * #%L
+ * timefractalweb-data-user
+ * %%
+ * Copyright (C) 2011 - 2018 Advanced Natural Research Institute
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
+import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
+import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToString;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@SuppressWarnings("serial")
+@Entity
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    public User() {
+    }
+
+    public User(String userName, String firstName, String lastName, String email) {
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+    
+    public String getUserName() {
+        return userName;
+    }
+    
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        return reflectionHashCode(this, false);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        User user = (User) obj;
+        return reflectionEquals(user, this, false);
+    }
+
+    @Override
+    public String toString() {
+        return reflectionToString(this);
+    }
+
+}
