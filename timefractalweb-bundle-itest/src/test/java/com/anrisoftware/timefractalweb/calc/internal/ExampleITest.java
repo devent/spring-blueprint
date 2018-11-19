b9ab0e4fe9783bdba28c62af3d02559d88e28dac
Erwin Müller <erwin.mueller@deventm.org>
2018-11-19 08:37:34 +0100

    Finish karaf i-test.

--- timefractalweb-bundle-itest/src/test/java/com/anrisoftware/timefractalweb/calc/internal/ExampleITest.java
+++ timefractalweb-bundle-itest/src/test/java/com/anrisoftware/timefractalweb/calc/internal/ExampleITest.java
@@ -1,15 +1,3 @@
-package com.anrisoftware.timefractalweb.calc.internal;
-
-import static org.ops4j.pax.exam.CoreOptions.maven;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.configureSecurity;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.logLevel;
-
-import java.io.File;
-
 /*-
  * #%L
  * timefractalweb-calc-itest
@@ -19,9 +7,9 @@ import java.io.File;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,6 +17,18 @@ import java.io.File;
  * limitations under the License.
  * #L%
  */
+package com.anrisoftware.timefractalweb.calc.internal;
+
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.configureSecurity;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.logLevel;
+
+import java.io.File;
 
 import org.apache.karaf.features.Feature;
 import org.apache.karaf.features.FeaturesService;
@@ -37,10 +37,12 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.ConfigurationManager;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.karaf.options.LogLevelOption;
 import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
+import org.ops4j.pax.exam.options.MavenUrlReference;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 
@@ -68,6 +70,9 @@ public class ExampleITest extends KarafTestSupport {
             localRepository = "";
         }
 
+        MavenUrlReference karafStandardRepo = maven().groupId("org.apache.karaf.features").artifactId("standard")
+                .version(karafVersion()).classifier("features").type("xml");
+
         return new Option[] {
                 // KarafDistributionOption.debugConfiguration("8889", true),
                 karafDistributionConfiguration().frameworkUrl(karafUrl).name("Apache Karaf")
@@ -80,12 +85,29 @@ public class ExampleITest extends KarafTestSupport {
                 mavenBundle().groupId("org.apache.servicemix.bundles")
                         .artifactId("org.apache.servicemix.bundles.hamcrest").versionAsInProject(),
                 mavenBundle().groupId("org.apache.karaf.itests").artifactId("common").versionAsInProject(),
+                mavenBundle().groupId("com.anrisoftware.timefractalweb").artifactId("timefractalweb-data-persistence")
+                        .versionAsInProject(),
+                mavenBundle().groupId("com.anrisoftware.timefractalweb")
+                        .artifactId("timefractalweb-spring-core-bundles").versionAsInProject(),
+                mavenBundle().groupId("com.anrisoftware.timefractalweb")
+                        .artifactId("timefractalweb-spring-data-bundles").versionAsInProject(),
+                mavenBundle().groupId("com.google.guava").artifactId("guava").versionAsInProject(),
+                mavenBundle().groupId("org.hibernate.javax.persistence").artifactId("hibernate-jpa-2.1-api")
+                        .versionAsInProject(),
+                mavenBundle().groupId("org.apache.commons").artifactId("commons-lang3").versionAsInProject(),
+                features(karafStandardRepo, "scr"),
                 editConfigurationFilePut("etc/org.ops4j.pax.web.cfg", "org.osgi.service.http.port", httpPort),
                 editConfigurationFilePut("etc/org.apache.karaf.management.cfg", "rmiRegistryPort", rmiRegistryPort),
                 editConfigurationFilePut("etc/org.apache.karaf.management.cfg", "rmiServerPort", rmiServerPort),
                 editConfigurationFilePut("etc/org.apache.karaf.shell.cfg", "sshPort", sshPort),
                 editConfigurationFilePut("etc/org.ops4j.pax.url.mvn.cfg", "org.ops4j.pax.url.mvn.localRepository",
                         localRepository) };
+    }
+
+    public static String karafVersion() {
+        ConfigurationManager cm = new ConfigurationManager();
+        String karafVersion = cm.getProperty("pax.exam.karaf.version", "4.2.0");
+        return karafVersion;
     }
 
     @Test
