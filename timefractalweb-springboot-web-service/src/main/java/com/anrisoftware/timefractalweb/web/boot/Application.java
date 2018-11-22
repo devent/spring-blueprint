cef8dc394df0f6499519df8085068be0457158db
Erwin Müller <erwin.mueller@deventm.org>
2018-11-22 16:46:56 +0100

    Add spring-boot for web-service.

--- timefractalweb-springboot-web-service/src/main/java/com/anrisoftware/timefractalweb/web/boot/Application.java
+++ timefractalweb-springboot-web-service/src/main/java/com/anrisoftware/timefractalweb/web/boot/Application.java
@@ -3,7 +3,7 @@ package com.anrisoftware.timefractalweb.web.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages= {"com.anrisoftware.timefractalweb.web.core.internal"})
+@SpringBootApplication(scanBasePackages= {"com.anrisoftware.timefractalweb.spring.webservice.internal"})
 public class Application {
     
     public static void main(String[] args) {
